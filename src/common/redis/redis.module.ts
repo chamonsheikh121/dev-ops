5206a66a22aef1c216499ef347ca23bac5dd9963
Chamon ali <studentchamon1212@gmail.com>
2026-01-17 04:27:53 +0600

    udpated

--- src/common/redis/redis.module.ts
+++ src/common/redis/redis.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
-import { RedisService } from './redis.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
-  providers: [RedisService],
-  exports: [RedisService],
+  imports: [
+    CacheModule.registerAsync<any>({
+      useFactory: async () => ({
+        store: await redisStore({
+          socket: {
+            host: process.env.REDIS_HOST || 'localhost',
+            port: parseInt(process.env.REDIS_PORT || '6379'),
+          },
+          ttl: 60 * 1000, // milliseconds
+        }),
+      }),
+      isGlobal: true,
+    }),
+  ],
+  providers: [],
+  controllers: [],
 })
 export class RedisModule {}
