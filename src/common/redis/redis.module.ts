894247b14f122f9428540e1daaeef2eb45a80b5e
chamonsheikh121 <studentchamon1212@gmail.com>
2026-01-18 04:39:40 +0600

    deployment try -1

--- src/common/redis/redis.module.ts
+++ src/common/redis/redis.module.ts
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
+
 import { Module } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
 import { redisStore } from 'cache-manager-redis-yet';
